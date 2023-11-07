fd2bf4c88e8598fd658e6bcca125ae0f7ff6a140
Shadrin Igor <igor@shadrins.ru>
2023-11-07 23:23:43 +0300

    Update client spells loading

--- src/client/java/ru/littleligr/magic/engine/LigreMagicEngineClient.java
+++ src/client/java/ru/littleligr/magic/engine/LigreMagicEngineClient.java
@@ -2,14 +2,13 @@ package ru.littleligr.magic.engine;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.minecraft.client.MinecraftClient;
 import ru.littleligr.magic.engine.utils.DataFolders;
 
 public class LigreMagicEngineClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ClientLifecycleEvents.CLIENT_STARTED.register((minecraftServer) -> {
-            LigreMagicEngine.SPELLS.load(minecraftServer.getResourceManager(), DataFolders.SPELLS.path, LigreMagicEngine.GSON_BUILDER);
-            LigreMagicEngine.FORMS.load(minecraftServer.getResourceManager(), DataFolders.FORMS.path, LigreMagicEngine.GSON_BUILDER);
-        });
+        LigreMagicEngine.SPELLS.load(MinecraftClient.getInstance().getResourceManager(), DataFolders.SPELLS.path, LigreMagicEngine.GSON_BUILDER);
+        LigreMagicEngine.FORMS.load(MinecraftClient.getInstance().getResourceManager(), DataFolders.FORMS.path, LigreMagicEngine.GSON_BUILDER);
     }
 }
